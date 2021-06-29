dd07fbe7e133379e3c9d667ef170d99d80a78f5e
Suraj Kushvaha <71623164+surajkushvaha@users.noreply.github.com>
2021-06-29 17:55:48 +0530

    Delete code/dist/code directory

--- code/dist/code/src/app/thirukkural-class.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ThirukkuralClass } from './thirukkural-class';
-
-describe('ThirukkuralClass', () => {
-  it('should create an instance', () => {
-    expect(new ThirukkuralClass()).toBeTruthy();
-  });
-});
